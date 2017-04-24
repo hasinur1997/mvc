32483385468cbfd86a99e25b19ca3e8baa1b99f4
unknown <hasinurrahman3@gmail.com>
2017-04-24 17:18:23 +0600

    query builder

--- app/core/Model.php
+++ app/core/Model.php
@@ -2,15 +2,58 @@
 class Model
 {
 	private $db;
+	
+	private $query;
+	
+	private $result;
+	
+	private $count;
 
 	public function __construct()
 	{
-
+		$this->db = Database::getInstance();
 	}
 
 
-	public static function query()
+	public static function query($sql, $params = [])
 	{
+		if($this->query = $this->db->prepare($sql)){
+			
+			if(count($params)){
+				
+				$x = 1;
+				
+				foreach($params as $param){
+					
+					$this->query->bindValue($x, $param);
+					
+					$x++;
+				}
+			}
+		}
 		
+		
+		if($this->query->execute()){
+			
+			$this->result = $this->query->fetchAll(PDO::FETCH_OBJ);
+			
+			$this->count = $this->query->rowCount();
+			
+		}else{
+			return false;
+		}
+	}
+	
+	
+	
+	
+	public function result()
+	{
+		return $this->result;
+	}
+	
+	public function count()
+	{
+		return $this->count;
 	}
 }
